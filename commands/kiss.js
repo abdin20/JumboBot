bf1fb8ae212170e6c775c47024a8d8730a523f71
naymul <naymul@hotmail.ca>
2020-03-08 04:14:17 -0400

    added more images

--- commands/kiss.js
+++ commands/kiss.js
@@ -1,11 +1,26 @@
 
 const fs = require('fs');
 const Discord = require('discord.js');
+
 module.exports = {
 	name: 'kiss',
-	description: '$kiss <user>',
+    description: '$kiss <user>',
+
+
 	async execute(message, args) {
         //if no arguments
+        
+        //random images uwu
+        files =[
+            "https://i.imgur.com/sGVgr74.gif",
+            "https://media.giphy.com/media/nyGFcsP0kAobm/giphy.gif",
+            "https://media2.giphy.com/media/FqBTvSNjNzeZG/giphy.gif",
+            "https://data.whicdn.com/images/328811537/original.gif",
+            "https://data.whicdn.com/images/294356906/original.gif",
+            "https://media1.giphy.com/media/12VXIxKaIEarL2/giphy.gif",
+            "https://media1.tenor.com/images/ac790bce799c45786b102e567173d115/tenor.gif?itemid=15517596"
+        
+        ]
         if(!args){
             message.reply("you need to mention a user!")
             return;
@@ -13,6 +28,7 @@ module.exports = {
 
         //SET THE TEXT TO SEND
         msg= `<@${message.author.id}> kissed ${message.mentions.users.first()}` 
-        message.channel.send(msg, {files: ["https://media.giphy.com/media/nyGFcsP0kAobm/giphy.gif"]});
+        file=files[Math.floor(Math.random() * (+files.length ))]
+        message.channel.send(msg, {files: [file]});
 	},
 };
