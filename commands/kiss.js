f0513c8b35201dca02b0c0b8e6cb2bf7afd4a0ce
naymul <naymul@hotmail.ca>
2020-03-10 02:52:58 -0400

    new random algorithm

--- commands/kiss.js
+++ commands/kiss.js
@@ -4,7 +4,6 @@ const Discord = require('discord.js');
 
 const imageSearch = require('image-search-google');
 const imageClient = new imageSearch(process.env.CSI, process.env.GOOGLE_API);
-const imageClientTwo = new imageSearch(process.env.CSI, process.env.GOOGLE_API);
 
 module.exports = {
     name: 'kiss',
@@ -20,32 +19,19 @@ module.exports = {
         }
 
         //search google
-        imageClient.search("anime kissing gif", {})
+        imageClient.search("anime kissing gif", {page : Math.floor( (Math.random()*10) + 1)})
             .then(images => {
                 //get a random image from the array
-                for (var k=0;k<images.length;k++){
-                    pictures.push(images[k].url);
-                }
-                
-            })
-            .catch(error => message.reply("Couldn't find any images"));
-
-            imageClientTwo.search("anime kissing gif", {page : 2})
-            .then(images => {
-                //get a random image from the array
-                for (var k=0;k<images.length;k++){
-                    pictures.push(images[k].url);
-                }
-                
-            })
-            .catch(error => message.reply("Couldn't find any images"));
-
-
-            image = (picutres[Math.floor(Math.random() * pictures.length)])
+                image = (images[Math.floor(Math.random() * images.length)])
 
                 //SET THE TEXT TO SEND
                 msg = `<@${message.author.id}> kissed ${message.mentions.users.first()}`
         
                 message.channel.send(msg, { files: [image] });
+            })
+            .catch(error => message.reply("Couldn't find any images"));
+
+
+           
     },
 };
