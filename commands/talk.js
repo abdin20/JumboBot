eedc19b40deb3675e962655178e74f04d1449fd9
naymul <naymul@hotmail.ca>
2020-04-01 14:54:48 -0400

    music works after talk

--- commands/talk.js
+++ commands/talk.js
@@ -1,6 +1,7 @@
 
 const fs = require('fs');
 const Discord = require('discord.js');
+var mongo = require("../mongodb.js");
 module.exports = {
 	name: 'talk',
 	description: 'talk',
@@ -9,7 +10,9 @@ module.exports = {
 
         //check if person in channel
         if (message.member.voice.channel) {
+            await mongo.deleteQueueByObject({ guildId: message.guild.id });
            await message.member.voice.channel.join()
+           
            .then(foo = async (connection) => {
                await connection.play("https://lithi.io/file/suMQ.mp3");
            });
