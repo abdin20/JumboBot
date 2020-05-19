c6151062caadfc5e73f7f7339b3906c01561e335
naymul <naymul@hotmail.ca>
2020-05-19 02:25:19 -0400

    play test

--- commands/play.js
+++ commands/play.js
@@ -175,11 +175,12 @@ module.exports = {
         //check if options argument was passed through
         if (typeof options != 'undefined') {
           seek = options.seconds; //set seek to options passed through
+          console.log("seek to " +seek +" seconds")
         } else {
           seek = 0;  //else set to 0
         }
 
-        const dispatcher = connection.play(ytdl(url),[{ filter: 'audioonly'}, seek+"s"]).on("finish", async () => {
+        const dispatcher = connection.play(ytdl(url),[{ filter: 'audioonly'}, {begin: seek+"s"}]).on("finish", async () => {
 
           //get the latest song queue
           results = await mongo.findQueueByGuildId(voiceChannel.guild.id)
