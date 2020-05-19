ea6ad23360742e93b6b37b1c82640910ad62ea2b
naymul <naymul@hotmail.ca>
2020-05-19 18:33:06 -0400

    added youtube playlist support

--- commands/playlist.js
+++ commands/playlist.js
@@ -6,7 +6,7 @@ var play = require("./play.js");
 
 module.exports = {
     name: 'playlist',
-    description: '<args> possible args "add <url/term>", "play <user>", <view> <user>',
+    description: '<args> possible args "add <url/term>", "play <user> <num of songs>", <view> <user>, <remove> <song>',
     async execute(message, args) {
         //display nicely with embeds
         exampleEmbed = new Discord.MessageEmbed();
@@ -22,9 +22,9 @@ module.exports = {
             }
             //get rid of the defining argument, and get the parameters
             args.shift();
-            song =args.join(" ")
-            console.log("adding " +song + " to playlist");
-            
+            song = args.join(" ")
+            console.log("adding " + song + " to playlist");
+
             //check to see if user has playlist, if not then make one
             results = await mongo.findPlaylistById(message.author.id);
 
@@ -44,17 +44,29 @@ module.exports = {
             }
 
             //send confirmation 
-            exampleEmbed.setDescription("Added "+ song +" to your playlist");
+            exampleEmbed.setDescription("Added " + song + " to your playlist");
             message.channel.send(exampleEmbed);
         }
 
 
         //playing a random song from someones playlist
         if (args[0] === "play") {
+            var songCount = 0;
+            var songsArray = new Array();
+            var songCount = 3;
             if (typeof message.mentions.users.first() === 'undefined') {
                 exampleEmbed.setDescription("you need to mention someone to play their playlist");
                 message.channel.send(exampleEmbed);
                 return;
+            }
+
+            //check if number is valid
+            if (isNaN(args[2])) {
+                exampleEmbed.setDescription("Added 3 songs from " + message.mentions.users.first().username + "'s playlist");
+                message.channel.send(exampleEmbed);
+
+            } else {
+                songCount = args[2];
             }
 
             //check if in voice channel
@@ -75,24 +87,42 @@ module.exports = {
                 return;
             } else {
 
-                //if exists choose random song
-                var song = results.songs[Math.floor(Math.random() * results.songs.length)];
-                //add to queue
-                play.execute(message,[song]); //in play loop now
+                //if exists 
 
+                //check if enough songs in playlist
+                if (results.songs.length < songCount) {
+                    exampleEmbed.setDescription("This person only has " + results.songs.length+ " songs");
+                    message.channel.send(exampleEmbed);
+                    return;
+                }
+
+                //loop until enough songs have been added
+                for (let m = 0; m < songCount; m++) {
+
+                    //get a random song
+                    song = results.songs[Math.floor(Math.random() * results.songs.length)];
+                    //whole the array contains it keep randomizing;
+                    while (songsArray.includes(song)) {
+                        song = results.songs[Math.floor(Math.random() * results.songs.length)];
+                    }
+
+                    //when it doesnt have add to array
+                    songsArray.push(song);
+                    play.execute(message, [song]) //and add to queue
+                }
             }
         }
 
-        if (args[0] === "view") { 
+        if (args[0] === "view") {
 
-             //if no one is mentioned view your own playlist by default.
-            var username =message.author.username;
-            var id=message.author.id;
+            //if no one is mentioned view your own playlist by default.
+            var username = message.author.username;
+            var id = message.author.id;
 
             //check to see mentions
             if (typeof message.mentions.users.first() !== 'undefined') {
-                username=message.mentions.users.first().username;
-                id=message.mentions.users.first().id;  //set id and username accordingly 
+                username = message.mentions.users.first().username;
+                id = message.mentions.users.first().id;  //set id and username accordingly 
             }
 
             //find playlist
@@ -101,58 +131,58 @@ module.exports = {
 
             //if not exists then send message
             if (!results) {
-                exampleEmbed.setDescription(username+ " has no playlist ");
+                exampleEmbed.setDescription(username + " has no playlist ");
                 message.channel.send(exampleEmbed);
                 return;
-            }else{
+            } else {
                 //go through songs
-                for(let m=0;m<results.songs.length;m++){
-                    exampleEmbed.addField(m+1,results.songs[m],true); //add fields for each song
+                for (let m = 0; m < results.songs.length; m++) {
+                    exampleEmbed.addField(m + 1, results.songs[m], true); //add fields for each song
                 }
 
                 message.channel.send(exampleEmbed); ///send to  channel
-                
+
             }
         }
 
-        if (args[0] === "remove") { 
+        if (args[0] === "remove") {
             if (typeof args[1] === 'undefined') { //check for url/term to remove 
                 exampleEmbed.setDescription("you need an url/search term to remove from your playlist");
                 message.channel.send(exampleEmbed);
                 return;
             }
 
-             //check to see if playlist exists
-             results = await mongo.findPlaylistById(message.author.id);
+            //check to see if playlist exists
+            results = await mongo.findPlaylistById(message.author.id);
 
-             //if not exists thwen send message
-             if (!results) {
-                 exampleEmbed.setDescription("You have no playlist ");
-                 message.channel.send(exampleEmbed);
-                 return;
-             } else {
-            //get rid of the defining argument, and get the parameters
-            args.shift();
-            removeSong =args.join(" ") //song we want to remove
-            console.log("removing :" + removeSong)
-            newSongs= new Array();
+            //if not exists thwen send message
+            if (!results) {
+                exampleEmbed.setDescription("You have no playlist ");
+                message.channel.send(exampleEmbed);
+                return;
+            } else {
+                //get rid of the defining argument, and get the parameters
+                args.shift();
+                removeSong = args.join(" ") //song we want to remove
+                console.log("removing :" + removeSong)
+                newSongs = new Array();
 
                 //loop through
-                for(let k=0;k<results.songs.length;k++){
+                for (let k = 0; k < results.songs.length; k++) {
 
                     console.log(results.songs[k])
                     //only add songs if it isnt the one we want to remove
-                    if(results.songs[k]!=removeSong){
-                       newSongs.push(results.songs[k]);  //push to array
-                       console.log("found remove song: "+ removeSong)
+                    if (results.songs[k] != removeSong) {
+                        newSongs.push(results.songs[k]);  //push to array
+                        console.log("found remove song: " + removeSong)
                     }
                 }
-                console.log("New song array "+newSongs)
+                console.log("New song array " + newSongs)
                 //update db
                 await mongo.updatePlaylistById(message.author.id, { songs: newSongs })
 
- 
-             }
+
+            }
 
         }
 
---
+++ commands/youtube.js
@@ -0,0 +1,95 @@
+//d imports
+var mongo = require("../mongodb.js");
+const Discord = require('discord.js');
+var play = require("./play.js");
+
+const vid_data = require('vid_data');
+
+
+
+module.exports = {
+  name: 'youtube',
+  description: '$youtube <playlist_link>',
+  async execute(message, args) {
+    //display nicely with embeds
+    exampleEmbed = new Discord.MessageEmbed();
+    exampleEmbed.setColor('#0099ff');
+    exampleEmbed.setTitle("Youtube Playlist");
+
+    var searchUrl = "";  //default to no url and indexing starting at 0
+    var index = 0;
+    var count = 0;
+    var songs = new Array();
+    //check if arguemnts are there
+    if (args.length < 1) {
+      exampleEmbed.setDescription("Please enter a playlist link");
+      message.channel.send(exampleEmbed);
+      return;
+    }
+
+    //check if in voice channel
+    if (!message.member.voice.channel) {
+      exampleEmbed.setDescription("You need to be in a voice channel");
+      message.channel.send(exampleEmbed);
+      return;
+    }
+
+    searchUrl = args[0];
+
+    //check if there is an index spot to start from
+    if (searchUrl.indexOf("&index=") > -1) {
+      index = searchUrl.substring(searchUrl.indexOf("&index=") + 7) //get time stamp part of url
+      console.log("index detected : " + index);
+    }
+
+    //check if there is an index spot to start from other form
+    if (searchUrl.indexOf("?index=") > -1) {
+      index = searchUrl.substring(searchUrl.indexOf("?index=") + 7) //get time stamp part of url
+      console.log("index detected : " + index);
+    }
+
+    //get vid ids for playlist
+    searchResults = await vid_data.get_playlist_videos(searchUrl)
+
+    if (!searchResults) {
+      exampleEmbed.setDescription("error with playlists");
+      message.channel.send(exampleEmbed);
+      return;
+    }
+    //add the videos to playlist
+    for (var m = index - 1; m < searchResults.length; m++) {
+      count++;
+      songs.push("https://www.youtube.com/watch?v=" + searchResults[m]);
+    }
+
+    results = await mongo.findQueueByGuildId(message.guild.id);
+
+    //queue not found
+    if (!results) {
+      propertyObject = new Object();
+      propertyObject.guildId = message.guild.id
+      propertyObject.songs = songs
+      propertyObject.loop = false; //loop defaults to off
+
+      //create queue in db
+      await mongo.createQueueByObject(propertyObject)
+
+      //go to playmusic function
+      await play.playMusic(message, message.member.voice.channel);
+
+    } else {
+      //get the songs from queue and concat the new playlist songs
+      newSongs = results.songs.concat(songs);
+
+      //update queue
+      await mongo.updateQueueByGuildId(message.guild.id, { songs: newSongs })
+
+    }
+
+
+    //send embed message
+    exampleEmbed.setDescription("Added " + count + " songs to queue");
+    message.channel.send(exampleEmbed);
+
+  },
+};
