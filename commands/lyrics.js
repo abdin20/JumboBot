b0b880fc51fd2993329a0674940d6d8991d55723
naymul <naymul@hotmail.ca>
2020-05-20 19:59:01 -0400

    possible debug

--- commands/lyrics.js
+++ commands/lyrics.js
@@ -71,7 +71,7 @@ module.exports = {
                     console.error(err);
                   });
 
-                  if(result.result!==null || result.result.length>0){
+                  if(result.result!==null){
                       break;
                   }
                   workingTitleArray.pop();
