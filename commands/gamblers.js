e24111e82af5238994d3f9093466fa4eb935bbc7
abdin20 <naymul@wowa.ca>
2022-06-15 19:37:24 -0400

    fixed jav

--- commands/gamblers.js
+++ commands/gamblers.js
@@ -12,10 +12,10 @@ module.exports = {
         exampleEmbed.setColor('#0099ff');
         exampleEmbed.setTitle("Jumbo");
             //riley, noah, jack w, javier
-        msg =`<@152558158806646784> <@331589423546368001> <@559892149307572234> <@967261754998542366>`
+        msg =`<@152558158806646784> <@331589423546368001> <@559892149307572234> <@166703160876990464>`
             //jet , abdn, shane, jason, milton
         msg +=`<@134127232904986624> <@163368896844267521> <@545042126644445184> <@152989214932336640> <@199708115434864641>`
-        msg+=  `ATTENTION ALL GAMBLERS`
+        msg+=  `\n ATTENTION ALL DISTINGUISHED GENTLEMEN`
         
         exampleEmbed.setDescription(msg)
         message.channel.send(exampleEmbed);;
